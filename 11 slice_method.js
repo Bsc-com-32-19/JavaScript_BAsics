ee5facd644fed42ce1894d48caf7f311a57c2d66
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-24 10:03:13 +0200

    added slice_ methods

---
+++ 11 slice_method.js
@@ -0,0 +1,16 @@
+//slice() method extracts a section of a string and 
+//returns it as a newstring, 
+// without modifying the original string.
+
+let fullName = "Mirriam Yambeni";
+let firstName;
+let lastName;
+
+// firstName = fullName.slice(0, 7);
+// lastName = fullName.slice(7);
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName - fullName.slice(fullName.indexOf(" " ) );
+
+console.log(firstName);
+console.log(lastName);
