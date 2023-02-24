4367c85028b56ab3b3c69306d5baf8e559867f41
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-24 10:28:00 +0200

    added method_chaining

---
+++ 12 method_chaining.js
@@ -0,0 +1,6 @@
+//method chaining means calling one method after another 
+// in one continous line of code
+
+let userName = "mimie";
+let letter = userName.charAt(0).toUpperCase();
+console.log(letter);
