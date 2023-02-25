049714b4a8ea1c193e39ac787cb50144e1c876da
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-25 16:02:06 +0200

    added NOT_logical_operator

---
+++ 18 not logical operator.js
@@ -0,0 +1,29 @@
+// ! NOT logical operator 
+//typically used to reverse a condition's boolean value 
+//true -> false& false -> true
+
+let temp = 50;
+let sunny = true;
+ if (temp > 0){
+    console.log("It's warm outside");
+ }
+ else{ 
+    console.log("It's cold outsede");
+ }
+
+ let tempe = -15;
+ if (!(temp > 0)){
+   console.log("It's warm outside");
+ } 
+
+ else {
+   console.log("It's cold outsede");
+ }
+
+ if(!sunny){
+    console.log("It's cloudy outside");
+ }
+
+ else{
+    console.log("It's  sunny outside");
+ }
