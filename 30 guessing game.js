5f5c31392289aa6b160f3b14471efe3285f9b490
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-03-01 17:07:01 +0200

    temperature_conversion

--- 30 guessing game.js
+++ 30 guessing game.js
@@ -1,19 +1,18 @@
 const answer = Math.floor(Math.random() * 10 + 1);
-esses = 0;
+guesses = 0;
 
 document.getElementById("submitButton").onclick = function(){
 
+     let guess = document.getElementById("guessField").value
+     guesses+=1;
 
-let guess = document.getElementById("guessField").value
-guess+=1;
-
-if(guess == answer){
+      if(guess == answer){
     alert(`${answer} is the #. It took you ${guesses} guess`)
-}
-else if(guess < answer){
+     }
+    else if(guess < answer){
     alert("Too small");
-}
- else{
+   }
+   else{
     alert("Too large!");
  }  
 
